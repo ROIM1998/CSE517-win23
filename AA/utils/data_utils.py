af914f4b981837dd14e7ff1bbb11988713177b52
ROIM1998 <roimgame@gmail.com>
2023-01-17 08:40:54 +0800

    Reform data utils, and exclude OOV

--- AA/utils/data_utils.py
+++ AA/utils/data_utils.py
@@ -28,13 +28,17 @@ def build_data(data, dev_ratio: float = 0.1, test_ratio: float = 0.1):
     eval_data, test_data, train_data = data[:dev_test_sep], data[dev_test_sep:test_train_sep], data[test_train_sep:]
     return train_data, eval_data, test_data
 
-def text_to_feature(text: str):
+def preprocess_text(text: str):
     # Removing punctuations
     text = text.translate(str.maketrans('', '', string.punctuation))
     # Converting all words to lower cases
     text = text.lower()
     # Converting newline into spaces, and separate by space for tokenization
     text = text.replace('\n', ' ').split()
+    return text
+
+def text_to_feature(text: str):
+    text = preprocess_text(text)
     for word in set(text):
         if word not in _vocab_cnt:
             _vocab_cnt[word] = 1
@@ -44,22 +48,21 @@ def text_to_feature(text: str):
             _vocab_cnt[word] += 1
     return dict(Counter(text))
 
-def data_to_feature(data):
+def data_to_feature(data, cutoff=50):
     global _vocab_cnt, _vocab_to_id, _vocab
     # Extracting TF_IDF feature from raw texts
     data_with_cnt = [(text_to_feature(x), y) for x, y in tqdm(data)]
-    _vocab = ['[OOV]']
-    _vocab_to_id = {'[OOV]': 0}
+    _vocab = []
+    _vocab_to_id = {}
     new_vocab_cnt = {}
     oov_cnt = 0
     for word, cnt in _vocab_cnt.items():
-        if cnt >= 50:
+        if cnt >= cutoff:
             _vocab.append(word)
             _vocab_to_id[word] = len(_vocab_to_id)
             new_vocab_cnt[word] = cnt
         else:
             oov_cnt += cnt
-    new_vocab_cnt['[OOV]'] = oov_cnt
     _vocab_cnt = new_vocab_cnt
     data_with_tfidf = [(np.array([(x[word] if word in x else 0) * np.log(len(data_with_cnt) / _vocab_cnt[word]) for word in _vocab]), y) for x, y in tqdm(data_with_cnt)]
-    return data_with_tfidf
+    return data_with_tfidf, _vocab, _vocab_cnt
