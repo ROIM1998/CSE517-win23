64716c0311ffa6aca5b21ddba6084c6a093ab1ca
ROIM1998 <roimgame@gmail.com>
2023-01-30 07:21:00 +0800

    Add bigram model

--- AB/models/ngram.py
+++ AB/models/ngram.py
@@ -43,3 +43,38 @@ class UnigramModel(NgramModel):
     def perplexity(self, dataset: List[np.ndarray]):
         ce_loss = np.mean([- np.log2(self.evaluate(s)) / len(s) for s in dataset])
         return 2 ** ce_loss
+    
+class BigramModel(NgramModel):
+    def __init__(self, eps=1):
+        self.theta = None
+        self.bigram_cnt = {}
+        self.eps = eps
+    
+    def train(self, train_dataset):
+        num_unique_tokens = len(np.unique(np.concatenate(train_dataset)))
+        for s in train_dataset:
+            for i in range(len(s) - 1):
+                if (s[i], s[i + 1]) not in self.bigram_cnt:
+                    self.bigram_cnt[(s[i], s[i + 1])] = 1 + self.eps
+                else:
+                    self.bigram_cnt[(s[i], s[i + 1])] += 1
+
+        self.theta = np.ones([num_unique_tokens, num_unique_tokens])
+        for (w1, w2), cnt in self.bigram_cnt.items():
+            self.theta[w1, w2] = cnt
+        self.theta = (self.theta.T / self.theta.sum(axis=1)).T
+                    
+    def predict_next_word(self, prefix):
+        return self.theta[prefix[-1]].argmin()
+    
+    def evaluate(self, sentence, eps=1e-300):
+        if isinstance(sentence, np.ndarray) and sentence.ndim == 1:
+            return np.prod([self.theta[sentence[i], sentence[i + 1]] for i in range(len(sentence) - 1)]) + eps
+        elif isinstance(sentence, list) or (isinstance(sentence, np.ndarray) and sentence.ndim == 2):
+            return np.mean([self.evaluate(s) for s in sentence]) + eps
+        else:
+            return None
+        
+    def perplexity(self, dataset: List[np.ndarray]):
+        ce_loss = np.mean([- np.log2(self.evaluate(s)) / len(s) for s in dataset])
+        return 2 ** ce_loss
