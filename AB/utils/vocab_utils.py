edc7942876582f28f37e55299121a4539cc1d3f5
ROIM1998 <roimgame@gmail.com>
2023-02-03 04:55:42 +0800

    Add tokenizer for text classifications

--- AB/utils/vocab_utils.py
+++ AB/utils/vocab_utils.py
@@ -1,10 +1,10 @@
 import numpy as np
 from .data_utils import read_file, load_glove_model
-from typing import List
+from typing import List, Optional
 from collections import Counter
 
 class Vocab:
-    def __init__(self, corpus_path: str = None, glove_path: str = None, cutoff_frequency: int = 3):
+    def __init__(self, corpus_path: str = None, glove_path: str = None, cutoff_frequency: int = 3, to_lower: bool = False, unk_token: Optional[str] = None, stop_token: Optional[str] = None, start_token: Optional[str] = None):
         self.corpus_path = corpus_path
         self.glove_path = glove_path
         self.glove_weights = None
@@ -21,8 +21,12 @@ class Vocab:
                 self._vocab_to_id[k] = len(self._vocab_to_id)
                 self.glove_embeddings.append(v)
             self.glove_embeddings = np.stack(self.glove_embeddings)
+        for token in [unk_token, stop_token, start_token]:
+            if token:
+                self._vocab.append(token)
+                self._vocab_to_id[token] = len(self._vocab_to_id)
         if corpus_path:
-            lines = read_file(corpus_path, to_lower=True)
+            lines = read_file(corpus_path, to_lower=to_lower)
             corpus_vocab_cnt = dict(Counter([w for l in lines for w in l.split()]))
             for k, v in corpus_vocab_cnt.items():
                 if v >= cutoff_frequency and k not in self._vocab_to_id:
@@ -43,3 +47,25 @@ class Vocab:
             return self.glove_weights[idx]
         else:
             return self.glove_embeddings[idx]
+        
+class Tokenizer:
+    def __init__(self, corpus_path: str = None, glove_path: str = None, cutoff_frequency: int = 3, to_lower: bool = False, unk_token: Optional[str] = None, stop_token: Optional[str] = None, start_token: Optional[str] = None, pad_stop: bool = False, pad_start: bool = False):
+        self.vocab = Vocab(corpus_path, glove_path, cutoff_frequency, to_lower, unk_token, stop_token, start_token)
+        self.to_lower = to_lower
+        self.unk_token = unk_token
+        self.stop_token = stop_token
+        self.start_token = start_token
+        self.pad_stop = pad_stop
+        self.pad_start = pad_start
+        if pad_stop and not stop_token:
+            raise ValueError("pad_stop is set to True but no stop_token is provided")
+        if pad_start and not start_token:
+            raise ValueError("pad_start is set to True but no start_token is provided")
+        
+    def tokenize(self, text: str):
+        if self.to_lower:
+            text = text.lower()
+        return ([self.vocab._vocab_to_id[self.start_token]] if self.pad_start else []) + [self.vocab._vocab_to_id[w] if w in self.vocab._vocab_to_id else self.vocab._vocab_to_id[self.unk_token] for w in text.split()] + ([self.vocab._vocab_to_id[self.stop_token]] if self.pad_stop else [])
+    
+    def __call__(self, text: str):
+        return self.tokenize(text)
